0473489abfc8bc1f85c49feec8c5db1a71388da8
Stefan Langenbach <stefan.buenten@gmail.com>
2020-04-07 08:33:07 +0200

    Fix save_fig function; reformat file

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -15,18 +15,23 @@ def save_fig(plot, figure_path: Path, figure_name: str, file_format: str = "svg"
     """
     # get subplot object if it exists
     if type(plot) == np.ndarray:
-        plot = plot[0]
-        
+
+        # get nested subplot, e.g. if layout contains two columns
+        if type(plot[0]) == np.ndarray:
+            plot = plot[0][0]
+        else:
+            plot = plot[0]
+
     plot.get_figure().savefig(figure_path.joinpath(f"{figure_name}.{file_format}"), format=file_format, dpi=dpi)
-    
+
 
 class MLPipeline:
     """
     Wrapper class for running MLflow boosted sklearn pipelines.
     """
-    
-    def __init__(self, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series, 
-                model_path: Path, figure_path: Path, mlflow_path: str, mlflow_experiment: str):
+
+    def __init__(self, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series,
+                 model_path: Path, figure_path: Path, mlflow_path: str, mlflow_experiment: str):
         self.X_train = X_train
         self.y_train = y_train
         self.X_test = X_test
@@ -38,19 +43,19 @@ class MLPipeline:
 
         # config Mlflow
         mlflow.set_tracking_uri(self.mlflow_path)
-        mlflow.set_experiment(self.mlflow_experiment) 
+        mlflow.set_experiment(self.mlflow_experiment)
 
         # use seaborn styling for plots
         mpl.style.use("seaborn-colorblind")
         mpl.rcParams.update({
-            # set figure size to A5 landscape for all plots
-            "figure.figsize": (8.3, 5.8),
-            "axes.labelsize": "large",
-            "axes.titlesize": "xx-large",
-            "xtick.labelsize": "large",
-            "ytick.labelsize": "large",
+            # set figure size to rotated A5 paper size for all plots
+            "figure.figsize": (5.8, 4.1),
+            "figure.titlesize": "large",
+            "axes.labelsize": "medium",
+            "axes.titlesize": "x-large",
+            "xtick.labelsize": "small",
+            "ytick.labelsize": "small",
             "axes.grid": False})
-
 
     def run_pipeline(self, pipe, features: list, model_name: str, cv: int = 5, save_model: bool = False) -> dict:
         """
@@ -65,7 +70,7 @@ class MLPipeline:
         :returns: Dict containing sklearn model object and predictions on self.X_test
         """
         with mlflow.start_run():
-            
+
             # define pipeline, fit model and predict
             print(f"Running pipeline for '{model_name}' model")
             model = pipe.fit(self.X_train[features], self.y_train)
@@ -74,29 +79,29 @@ class MLPipeline:
 
             # calculate cross-validated metrics
             cv_acc_train = np.average(cross_val_score(model, self.X_train[features], y_pred_train,
-                                                scoring="accuracy", cv=cv, n_jobs=-1))
+                                                      scoring="accuracy", cv=cv, n_jobs=-1))
             cv_acc_test = np.average(cross_val_score(model, self.X_test[features], y_pred_test,
-                                                scoring="accuracy", cv=cv, n_jobs=-1))
+                                                     scoring="accuracy", cv=cv, n_jobs=-1))
             cv_f1_train = np.average(cross_val_score(model, self.X_train[features], y_pred_train,
-                                                scoring="f1", cv=cv, n_jobs=-1))
-            cv_f1_test = np.average(cross_val_score(model, self.X_test[features], y_pred_test, 
-                                                scoring="f1", cv=cv, n_jobs=-1))
-            
+                                                     scoring="f1", cv=cv, n_jobs=-1))
+            cv_f1_test = np.average(cross_val_score(model, self.X_test[features], y_pred_test,
+                                                    scoring="f1", cv=cv, n_jobs=-1))
+
             # print results
             print(
                 f"CV accuracy train: {np.round(cv_acc_train, 4)}",
                 f"CV accuracy test: {np.round(cv_acc_test, 4)}",
-                f"CV F1-score train: {np.round(cv_f1_train, 4)}", 
-                f"CV F1-score test: {np.round(cv_f1_test, 4)}", 
+                f"CV F1-score train: {np.round(cv_f1_train, 4)}",
+                f"CV F1-score test: {np.round(cv_f1_test, 4)}",
                 sep="\n")
-        
+
             print("Classification report")
             print(classification_report(self.y_test, y_pred_test))
-            
+
             # print confusion matrix
             if not "encode" in model.named_steps.keys():
-                plot_confusion_matrix(model.named_steps["model"], self.X_test[features], self.y_test, cmap="Blues", 
-                    normalize=None, values_format="d")
+                plot_confusion_matrix(model.named_steps["model"], self.X_test[features], self.y_test, cmap="Blues",
+                                      normalize=None, values_format="d")
 
             # print feature importances
             if hasattr(model.named_steps["model"], "feature_importances_"):
@@ -104,22 +109,22 @@ class MLPipeline:
                 feat_imp_plot = self._plot_feature_importances(feat_importances)
                 save_fig(feat_imp_plot, self.figure_path, f"{model_name}_feat_importances_plot")
                 mlflow.log_artifact(self.figure_path.joinpath(f"{model_name}_feat_importances_plot.svg"))
-            
+
             # log params, metrics, artifacts and model
             mlflow.log_params(model.named_steps["model"].get_params())
             mlflow.log_metrics({
                 "cv_accuracy_train": cv_acc_train,
                 "cv_accuracy_test": cv_acc_train,
-                "cv_f1_score_train": cv_f1_train, 
+                "cv_f1_score_train": cv_f1_train,
                 "cv_f1_score_test": cv_f1_train})
             mlflow.sklearn.log_model(model, model_name)
-            
+
             if save_model:
                 mlflow.sklearn.save_model(model, self.model_path.joinpath(model_name))
-            
+
             result = {"model": model, "y_pred": y_pred_test}
             return result
-    
+
     @staticmethod
     def _get_feature_importances(features: list, model) -> list:
         """
@@ -142,5 +147,5 @@ class MLPipeline:
         df = pd.DataFrame.from_records(feature_importances, columns=["feature", "importance"])
         plot = df.plot(kind="barh", x="feature", y="importance", legend=False, grid=True, title="Feature importances")
         plot.set_xlabel("importance")
-        
+
         return plot
